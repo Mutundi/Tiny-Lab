e47f09c0793d8a40b6f5afa8e7d1634d3179e8dd
Mutundi <levimutundi@gmail.com>
2024-04-13 21:12:27 +0300

    Age calculator

---
+++ age-calculator/index.js
@@ -0,0 +1,13 @@
+ const btnEl = document.getElementById("btn");
+ const birthdayEl = document.getElementById("birthday");
+ const resultEl = document.getElementById("result");
+
+ const calculateAge = () => {
+    const birthdayValue = birthdayEl.value;
+
+    if (birthdayValue === 0) {
+        alert("Please enter your birthday.")
+    }else{
+        
+    }
+ };

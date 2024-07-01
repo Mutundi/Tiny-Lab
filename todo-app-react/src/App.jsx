11ad8b10ebd7dfd8a8016c76f061cc1905846939
Mutundi <levimutundi@gmail.com>
2024-07-01 20:37:26 +0300

    Updated

---
+++ todo-app-react/src/App.jsx
@@ -0,0 +1,37 @@
+import react, {useState} from 'react';
+import './App.css'
+
+const App = () => {
+  //Leverage useState to manage state.
+  //todos is a variable that stores todo items
+  //setTodos is the function that manage the state in todos variable.
+
+  const [todos, setTodos] = useState([]);
+
+  //A function to add a todo item.
+  const addTodo = (todo) => {
+    setTodos([...todos, {text: todo, completed: false}]);
+  }
+
+  //A function to toggle complete
+  const toggleComplete = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].complete = !newTodos[index].complete;
+    setTodos(newTodos);
+  };
+
+  //A function to delete a todo item
+  const deleteTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="container mx-auto p-4 max-w-md">
+      <h1 className="text-3xl font-bold text-center mb-4">ToDo App</h1>
+
+    </div>
+  );
+}
+export default App
---
+++ todo-app-react/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html,js,jsx}",
+    "./components/**/*.{html,js,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
